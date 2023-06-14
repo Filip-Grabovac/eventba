99ec1acf090b91269e6046aa93fdb692d074fd75
Filip-Grabovac <filipgrabovac55@gmail.com>
2023-06-14 12:33:25 +0200

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">test</div>
+          <div className="col-lg-6">test</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
