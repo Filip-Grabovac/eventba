6f6739c69e4fd7f66a62d4bbc9031f2617028869
Geeraz <13kreso@gmail.com>
2023-06-24 18:56:41 +0200

    User routes fix

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const { getAllUsers, createUser, findUser } = require("../controllers/users");
 
-router.route("/users/").get(getAllUsers).post(createUser);
-router.route("/users/:type/:value").get(findUser);
+router.route("/").get(getAllUsers).post(createUser);
+router.route("/:type/:value").get(findUser);
 
 module.exports = router;
 // Routes
