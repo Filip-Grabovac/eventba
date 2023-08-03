e552d51a0dd4a2d688d3ed7d6c62ca6d4d6915e7
Geeraz <13kreso@gmail.com>
2023-08-03 11:07:29 +0200

    k

--- client/src/pages/profile-page/add-tickets/AddTickets.js
+++ client/src/pages/profile-page/add-tickets/AddTickets.js
@@ -79,31 +79,33 @@ export const AddTickets = ({ organizerEvents }) => {
       <div className="concert-container">
         {/* Render the concert data */}
         {concertData && (
-          <div className="top-part">
-            <div className="info">
-              <h4>{concertData.performer_name}</h4>
-              <p style={{ textTransform: "capitalize" }}>
-                {new Date(concertData.time_of_event).toLocaleDateString(
-                  "hr-HR",
-                  hrTimeFormat
-                )}{" "}
-                - {concertData.place.place}, {concertData.place.city},
-                {concertData.place.country}
-              </p>
+          <>
+            <div className="top-part">
+              <div className="info">
+                <h4>{concertData.performer_name}</h4>
+                <p style={{ textTransform: "capitalize" }}>
+                  {new Date(concertData.time_of_event).toLocaleDateString(
+                    "hr-HR",
+                    hrTimeFormat
+                  )}{" "}
+                  - {concertData.place.place}, {concertData.place.city},
+                  {concertData.place.country}
+                </p>
+              </div>
+              <img
+                className="info-buy-page-image"
+                src={
+                  concertData?.poster?.landscape
+                    ? `${process.env.REACT_APP_API_URL}/static/event-images/${concertData.poster.landscape}`
+                    : ""
+                }
+                alt="concertData.poster.landscape"
+              />
             </div>
-            <img
-              className="info-buy-page-image"
-              src={
-                concertData?.poster?.landscape
-                  ? `${process.env.REACT_APP_API_URL}/static/event-images/${concertData.poster.landscape}`
-                  : ""
-              }
-              alt="concertData.poster.landscape"
-            />
-          </div>
+            <TicketGen />
+          </>
         )}
       </div>
-      <TicketGen />
     </>
   );
 };
